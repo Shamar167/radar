de80e3cd06c4b72de646c72163dff7b0b9f6f5f7
zmclaughlin <zmclaughlin@localhost>
2026-01-20 10:20:37 -0500

    chore: make compile.sh configurable (build/upload, remove sudo)

--- compile.sh
+++ compile.sh
@@ -1,1 +1,14 @@
-sudo arduino-cli compile --fqbn arduino:avr:uno --upload -p /dev/ttyACM0 radar.ino
+#!/bin/sh
+# Usage: ./compile.sh [build|upload] [PORT]
+# Default: upload to /dev/ttyACM0
+
+FQBN="arduino:avr:uno"
+PORT="${2:-/dev/ttyACM0}"
+MODE="${1:-upload}"
+
+if [ "$MODE" = "build" ]; then
+	arduino-cli compile --fqbn "$FQBN" radar.ino
+else
+	arduino-cli compile --fqbn "$FQBN" --upload -p "$PORT" radar.ino
+fi
+
